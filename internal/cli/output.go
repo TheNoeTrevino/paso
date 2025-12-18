eeba4fc725bec503b99b7072bd5081d2c1c21161
TheNoeTrevino <noetrevino.music@gmail.com>
2025-12-18 00:55:59 -0600

    add: output formatter

--- internal/cli/output.go
+++ internal/cli/output.go
@@ -1,3 +1,52 @@
 package cli
 
-// output.go contains OutputFormatter for JSON/quiet/human modes
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+)
+
+type OutputMode string
+
+const (
+	OutputModeHuman OutputMode = "human"
+	OutputModeJSON  OutputMode = "json"
+	OutputModeQuiet OutputMode = "quiet"
+)
+
+type OutputFormatter struct {
+	mode   OutputMode
+	writer io.Writer
+}
+
+func NewOutputFormatter(mode OutputMode) *OutputFormatter {
+	return &OutputFormatter{
+		mode:   mode,
+		writer: os.Stdout,
+	}
+}
+
+func (f *OutputFormatter) Print(message string) {
+	if f.mode == OutputModeQuiet {
+		return
+	}
+	fmt.Fprintln(f.writer, message)
+}
+
+func (f *OutputFormatter) PrintJSON(data interface{}) error {
+	if f.mode == OutputModeQuiet {
+		return nil
+	}
+	encoder := json.NewEncoder(f.writer)
+	encoder.SetIndent("", "  ")
+	return encoder.Encode(data)
+}
+
+func (f *OutputFormatter) PrintID(id string) {
+	if f.mode == OutputModeHuman {
+		fmt.Fprintf(f.writer, "ID: %s\n", id)
+	} else {
+		fmt.Fprintln(f.writer, id)
+	}
+}
